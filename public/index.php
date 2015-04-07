2cf61694d4d2f5e7aa11ccc7172e9004435e92be
JoshuaKimble <joshkimble@gmail.com>
2015-04-06 19:29:37 -0600

    a bunch of changes

--- public/index.php
+++ public/index.php
@@ -1,19 +1,5 @@
 <?php
-
-require '../vendor/autoload.php';
-
-$app = new \Slim\Slim();
-
-$app->get('/',function (){
-	require '../src/view/login.html';
-});
-
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
-
-$app->post('/auth',function (){
-	echo "username: ".$_POST('username')." password: ".$_POST('password');
-});
-
-$app->run();
+$bootstrap = realpath(
+    __DIR__.'..'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.
+    'Config'.DIRECTORY_SEPARATOR.'bootstrap.php'
+);
---
+++ src/Config/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+$autoLoader = realpath(
+    __DIR__.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.
+    'vendor'.DIRECTORY_SEPARATOR.'autoload.php'
+);
+
+/** @noinspection PhpIncludeInspection */
+require $autoLoader;
+
+\Slim\Slim::registerAutoloader();
+
+// Load server specific configuration data.  Should
+// check an environment variable load the appropriate
+// server configuration file.
+require 'config.php';
+
+$app = new \Slim\Slim(
+    $config['app']['slim-config']
+);
+
+$app->group('/api',function () use ($app){
+	$app->POST('/auth',function () use ($app){
+		$body = $app->request()->getBody();
+		echo $_POST['username'];
+		echo $_POST['password'];
+		echo $body;
+	});
+});
+
+//require file that defined the API.
+
+$app->run();
